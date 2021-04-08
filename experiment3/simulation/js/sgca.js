5ffd17e996c37bc7004887610172da00a55a8c34
jaison10 <jaisonj1010@gmail.com>
2021-04-08 21:52:44 +0530

    pull

--- experiment3/simulation/js/sgca.js
+++ experiment3/simulation/js/sgca.js
@@ -696,6 +696,7 @@ function placeEsc(id, top_px, left_px, height_px, width_px) {
     document.getElementById("esc_arm" + id).style.height = height_px;
     document.getElementById("esc_arm" + id).style.width = width_px;
     document.getElementById("esc_arm" + id).style.transition = "all 1.5s ease-in-out";
+    // Author: Jaison
     setTimeout(() => {
         console.log("hiding the hovering esc")
         document.getElementById("esc_arm" + id).style.visibility = "hidden";
@@ -703,6 +704,7 @@ function placeEsc(id, top_px, left_px, height_px, width_px) {
         document.getElementById("esc_wire" + id).style.visibility = "visible";
         document.getElementById("esc_signal" + id).style.visibility = "visible";
     }, 2000);
+    // Jaison End
     escSelected++;
     console.log(escSelected);
     if (escSelected >= 4) {
@@ -741,8 +743,14 @@ function connectEscMotor(n){
         
         document.getElementById("esc_arm_placed1").style.visibility = "hidden";
         
-        document.getElementById("esc_motor_connected").style.visibility = "visible";
-
+        document.getElementById("esc_motor_connected1").style.visibility = "visible";
+    }
+    else if(n==2){
+        document.getElementById("placedMotor2").style.visibility = "hidden";
+        
+        document.getElementById("esc_arm_placed2").style.visibility = "hidden";
+        
+        document.getElementById("esc_motor_connected2").style.visibility = "visible";
     }
 }
 
